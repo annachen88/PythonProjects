41dc2b191cb1ba98300e94a20e23d0d78f85be5a
annachen88 <32476353+annachen88@users.noreply.github.com>
2024-05-15 23:59:05 +0800

    Update LC238_product_except_self.py

--- projects/problem_solver/LC238_product_except_self.py
+++ projects/problem_solver/LC238_product_except_self.py
@@ -16,6 +16,27 @@ class Solution:
     ex:
     Input: nums = [1,2,3,4]
     Output: [24,12,8,6]
+
+    ---------------------------------------
+    sol 1: time complexity O(n), but take O(n) space complexity
+    nums = [1,2,3,4]
+    pre = [1, 2, 6, 24] = i 前面相乘結果
+    post = [24, 24, 12, 4] = i 後面相乘結果
+    result = [pre[i-1]*post[i+1]] -> if not exist-> default 1
+    result: [24,12,8,6] = [1*24, 1*12, 2*4, 6*1]
+
+    sol 2:
+    nums = [1,2,3,4]
+    start from the beginning(i) using prefix variable(default 1) then record into the result array
+        record array position -> i+1
+    start from the end(len(nums)) using postfix variable(default 1) then postfix(i) multiply position(i+1) into the
+    result array
+        record array position -> i = len(len(nums))
+    result = []
+    start from the beginning -> result = [1, 1, 2, 6] = [1(default, prefix=1), 1(prefix*result[0] =1*1
+    and update prefix=result[0]=1)), 2(prefix * nums[1]=1*2 and update prefix =result[1] = 2 ), 6(prefix * nums[2]=2*3
+    and update prefix =result[2] = 6 )]
+
     """
     def product_except_self(self, nums: List[int]) -> List[int]:
         pre = 1
