76740c7a73cd1dfef8206facdc0e8cbc88cd5c25
annachen88 <32476353+annachen88@users.noreply.github.com>
2024-05-16 00:42:58 +0800

    Update LC238_product_except_self.py

--- projects/problem_solver/LC238_product_except_self.py
+++ projects/problem_solver/LC238_product_except_self.py
@@ -16,28 +16,41 @@ class Solution:
     ex:
     Input: nums = [1,2,3,4]
     Output: [24,12,8,6]
-
     ---------------------------------------
-    sol 1: time complexity O(n), but take O(n) space complexity
+    sol 1: time complexity O(n), but take O(n) memory space complexity
     nums = [1,2,3,4]
     pre = [1, 2, 6, 24] = i 前面相乘結果
     post = [24, 24, 12, 4] = i 後面相乘結果
     result = [pre[i-1]*post[i+1]] -> if not exist-> default 1
     result: [24,12,8,6] = [1*24, 1*12, 2*4, 6*1]
-
-    sol 2:
+    ---------------------------------------
+    sol 2: time complexity O(n), memory space complexity O(1)
     nums = [1,2,3,4]
-    start from the beginning(i) using prefix variable(default 1) then record into the result array
-        record array position -> i+1
-    start from the end(len(nums)) using postfix variable(default 1) then postfix(i) multiply position(i+1) into the
-    result array
-        record array position -> i = len(len(nums))
-    result = []
-    start from the beginning -> result = [1, 1, 2, 6] = [1(default, prefix=1), 1(prefix*result[0] =1*1
-    and update prefix=result[0]=1)), 2(prefix * nums[1]=1*2 and update prefix =result[1] = 2 ), 6(prefix * nums[2]=2*3
-    and update prefix =result[2] = 6 )]
+    idea :
+        start from the beginning(i) using prefix variable(default 1) then record into the result array
+            record array position -> i+1
+        start from the end(len(nums)) using postfix variable(default 1) then postfix(i) multiply position(i+1) into the
+        result array
+            record array position -> i = len(len(nums))
 
+    Trace the running process
+    nums = [1,2,3,4]
+    result = []
+    start from the beginning -> result = [1, 1, 2, 6]
+    trace 0: given default prefix = 1
+    trace 1: prefix = 1, result = [1], update prefix = 1 (result[0])
+    trace 2: prefix = 1, result = [1, 1*1] = [1,1] = [prefix * nums[0]], update prefix = 1 (result[1])
+    trace 3: prefix = 1, result = [1, 1, 1*2] =  [prefix * nums[1]], update prefix = 2 (result[2])
+    trace 4: prefix = 2, result = [1, 1, 2, 2*3] =  [prefix * nums[2]], update prefix = 6 (result[3])
+    then start from the ending to the beginning
+    trace 5: given default postfix =1
+    trace 6: postfix =1, result = [1, 1, 2, 2*3*1] = [1, 1, 2, 6] = result[3]*postfix ,update postfix = 4( postfix * nums[3])
+    trace 7: postfix =4, result = [1, 1, 2*4, 6] = [1, 1, 8, 6] = result[2]*postfix, update postfix = 12 ( postfix * nums[2])
+    trace 8: postfix =12, result = [1, 1*12, 8, 6] = [1, 12, 8, 6] =result[1]*postfix, update postfix = 24 ( postfix * nums[1])
+    trace 9: postfix = 24, result = [1*24, 12, 8, 6] = result[0]*postfix ...
+    finish then return result
     """
+
     def product_except_self(self, nums: List[int]) -> List[int]:
         pre = 1
         ans = [1]
@@ -49,7 +62,7 @@ class Solution:
             ans[i] *= pre
             pre *= nums[i]
         return ans
-
+ 
 
 def main():
     nums1 = [1,2,3,4]
