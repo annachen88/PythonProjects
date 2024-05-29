df36e1430ebb1a82cd3bf11b8b0b225917cabf71
annachen88 <32476353+annachen88@users.noreply.github.com>
2024-05-30 00:03:51 +0800

    Update LC191_number_of_1_bits.py

--- projects/problem_solver/LC191_number_of_1_bits.py
+++ projects/problem_solver/LC191_number_of_1_bits.py
@@ -4,6 +4,7 @@ Name: Anna
 -------------------------
 """
 
+
 class Solution:
     """
     ex:
@@ -12,7 +13,6 @@ class Solution:
     Explanation:
     The input binary string 1011 has a total of three set bits.
     """
-
     # if input n = 2147483645 (1111111111111111111111111111101) then need to run 32 times
     # (time complexity)
     def hammingWeight(self, n: int) -> int:
@@ -22,12 +22,22 @@ class Solution:
             n = n >> 1  # shift 1 to the right -> 101 -> 10
         return total_count
 
+    # idea => iterate the n & (n-1) until to the zero ,and cont the times of iterate
+    # why ? the idea of n & (n-1) is to remove one bit
+    def hammingWeight2(self, n: int) -> int:
+        total_count = 0
+        while n:
+            n &= (n-1)  # n = n & (n-1)
+            total_count += 1
+        return total_count
+
 
 def main():
     a = 2147483645
-    # b = 2
     sol = Solution()
     result = sol.hammingWeight(a)
+    print(result)
+    result = sol.hammingWeight2(a)
     print(result)
 
 
