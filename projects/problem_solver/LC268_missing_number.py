f782deef031d0708defa40e77daa20fddb16ac08
annachen88 <32476353+annachen88@users.noreply.github.com>
2024-05-31 21:20:32 +0800

    Update LC268_missing_number.py

--- projects/problem_solver/LC268_missing_number.py
+++ projects/problem_solver/LC268_missing_number.py
@@ -16,12 +16,27 @@ class Solution:
     Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 
     """
+
+
     def missingNumber(self, nums: List[int]) -> int:
         input_total = (1 + len(nums)) * len(nums) // 2
-        print(input_total)
+        # print(input_total)
         for i in range(len(nums)):
             input_total -= nums[i]
         return input_total
+
+
+    def missingNumber2(self, nums: List[int]) -> int:
+        result = len(nums)
+        for i in range(len(nums)):
+            result += (i - nums[i])
+        return result
+    """
+    n -> [0-3 , 1-0, 2-1] ,total = -1
+    -1 + 3 = 2
+    因正常：[0,1,2]
+    異常  :[3,0,1]
+    """
 
 
 def main():
@@ -29,7 +44,8 @@ def main():
     sol = Solution()
     result = sol.missingNumber(n)
     print(result)
-
+    result = sol.missingNumber2(n)
+    print(result)
 
 
 if __name__ == "__main__":
