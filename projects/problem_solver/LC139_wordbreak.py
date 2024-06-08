9919a294a3b55379bcac5c15b793f0fb4ed17d6f
annachen88 <32476353+annachen88@users.noreply.github.com>
2024-06-09 01:33:43 +0800

    Update LC139_wordbreak.py

--- projects/problem_solver/LC139_wordbreak.py
+++ projects/problem_solver/LC139_wordbreak.py
@@ -14,20 +14,29 @@ class Solution:
     Explanation: Return true because "leetcode" can be segmented as "leet code".
 
     idea 1 : o(n * m)
+    iterate the dictionary
     find the first fit string, then split the string find another matched string ...
-
+    from the last character
     """
 
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-
+        # initial dynnamic programming array
+        dp = [False] * (len(s) + 1)     # default False
+        dp[len(s)] = True
+        for i in range(len(s) -1, -1, -1):  # from the last character
+            for w in wordDict:
+                if i + len(w) <= len(s) and s[i:i+len(w)] == w:
+                    dp[i] = dp[i] + dp[i + len(w)]
+                if dp[i]:
+                    break  # do not keep because it must contain
+        return dp[0]
 
 
 def main():
-    text1 = "abcde"
-    text2 = "ace"
+    text1 = "leetcode"
+    wordDict = ["leet","code"]
     sol = Solution()
-    result = sol.longestCommonSubsequence(text1, text2)
-    print(result)
+    print(sol.wordBreak(text1, wordDict))
 
 
 if __name__ == "__main__":
