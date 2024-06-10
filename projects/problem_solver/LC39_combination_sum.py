bab9ec2e8825bcd393616b82c9d9e9f1792f5129
annachen88 <32476353+annachen88@users.noreply.github.com>
2024-06-10 23:53:54 +0800

    first draft

---
+++ projects/problem_solver/LC39_combination_sum.py
@@ -0,0 +1,31 @@
+"""
+File: LC39_combination_sum.py
+Name: Anna
+-------------------------
+"""
+
+from typing import List
+
+
+class Solution:
+    """
+    Input: candidates = [2,3,6,7], target = 7
+    Output: [[2,2,3],[7]]
+    Explanation:
+    2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+    7 is a candidate, and 7 = 7.
+    These are the only two combinations.
+    """
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+
+def main():
+    text1 = "leetcode"
+    wordDict = ["leet","code"]
+    sol = Solution()
+    print(sol.wordBreak(text1, wordDict))
+
+
+if __name__ == "__main__":
+    main()
