c97176126d646e7fc86312805878cd209c859e4f
annachen88 <32476353+annachen88@users.noreply.github.com>
2024-06-18 00:51:50 +0800

    Update LC62_Unique_paths.py

--- projects/problem_solver/LC62_Unique_paths.py
+++ projects/problem_solver/LC62_Unique_paths.py
@@ -19,16 +19,26 @@ class Solution:
 
     idea:
     from right bottom side then sum up all the possible path
+    [1] value from [3] + [4]
+    [1][3]
+    [4]
     """
 
     def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+        for i in range(m-1):
+            newRow = [1] * n
+            for j in range(n-2, -1, -1):
+                newRow[j] = newRow[j+1] + row[j]
+            row = newRow
+        return row[0]
 
 
 
 def main():
-    s = "226"
+    # s = "226"
     sol = Solution()
-    print(sol.numDecodings(s))
+    print(sol.uniquePaths(3,2))
 
 
 if __name__ == "__main__":
