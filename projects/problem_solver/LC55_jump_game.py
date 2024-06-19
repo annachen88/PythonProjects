866a8d08fd759c447774bb66fcc937d27f6dc810
annachen88 <32476353+annachen88@users.noreply.github.com>
2024-06-20 00:29:14 +0800

    Update LC55_jump_game.py

--- projects/problem_solver/LC55_jump_game.py
+++ projects/problem_solver/LC55_jump_game.py
@@ -14,19 +14,31 @@ class Solution:
     Output: true
     Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
-    idea 1: brute decision tree
-    idea 2: greedy
+    Input: nums = [3,2,1,0,4]
+    Output: false
+    Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+    idea 1: brute decision tree -> time complexity O(n^n)
+    idea 2: greedy -> time complexity O(n)
             from the goal index to the first index, check if it can reach
+
     """
 
     def canJump(self, nums: List[int]) -> bool:
-        pass
+        goal = len(nums)-1
+        for i in range(len(nums)-1,-1,-1): # from last
+            if i + nums[i] >= goal:
+                goal = i
+        return True if goal == 0 else False
 
 
 def main():
-    nums = [3, 2, 1, 0, 4]
+    nums1 = [2,3,1,1,4]
+    nums2 = [3,2,1,0,4]
     sol = Solution()
-    result = sol.canJump(nums)
+    result = sol.canJump(nums1)
+    print(result)
+    result = sol.canJump(nums2)
     print(result)
 
 
